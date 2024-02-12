7304bc0d6f5cfdbcf20343a86852eee441d69cb3
Ali Raza <alirazaarif995@gmail.com>
2024-02-12 14:27:00 +0500

    Standerlized Utilities Added

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
 // Hamesha database s bt krty waqt laazmi try catch use krty
 // Hamesha Async await krty
+// Configuration set krni ho ya middleware ka kam krna ho to app.use ko use krnengy express 
+// (err , req, res , next) ------> next aik flag hai (During middleware)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-dotenv.config({path:'./env'})
+
+dotenv.config({ path: './env' })
+const PORT = process.env.PORT || 8000
 
 connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("Server listening on port " + PORT)
+        })
+    })
+    .catch((error) => {
+        console.log("Error in connectDB", error);
+
+    })
